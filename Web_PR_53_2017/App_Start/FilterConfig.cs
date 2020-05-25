71558e3634a5a8e1576ec2b39bea2a8858c82e21
sonjatomcic <sonjatomcic@gmail.com>
2020-05-25 18:33:26 +0200

    Add project files.

---
+++ Web_PR_53_2017/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Web_PR_53_2017
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Web_PR_53_2017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_PR_53_2017.MvcApplication" Language="C#" %>
