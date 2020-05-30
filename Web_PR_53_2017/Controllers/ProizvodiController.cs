4bd9f15ccee2c1dfff24cf1a58a013a9a636b709
sonjatomcic <sonjatomcic@gmail.com>
2020-05-30 03:05:54 +0200

    neregistrovani skoro uradjeno

--- Web_PR_53_2017/Controllers/ProizvodiController.cs
+++ Web_PR_53_2017/Controllers/ProizvodiController.cs
@@ -12,8 +12,91 @@ namespace Web_PR_53_2017.Controllers
         // GET: Proizvodi
         public ActionResult Index()
         {
-            List<Proizvod> proizvodi = (List<Proizvod>)HttpContext.Application["proizvodi"];
-            return View(proizvodi);
+            List<Proizvod> pr = (List<Proizvod>)HttpContext.Application["proizvodi"];
+            List<Proizvod> proizvodi = new List<Proizvod>();
+  
+            Korisnik korisnik = (Korisnik)Session["korisnik"];
+            if ((korisnik == null) || (korisnik != null && korisnik.Uloga.Equals(Uloga.KUPAC)))
+            {
+                proizvodi = pr.Where(p => p.BrojTegli > 0).ToList<Proizvod>();
+            }
+            else
+            {
+               proizvodi = pr;
+             }
+
+            ViewBag.Proizvodi = proizvodi;
+            return View();
         }
+
+        //GET: Proizvodi/Sortiraj
+        public ActionResult Sortiraj(string vrsta="NAZIV", string nacin="RASTUCE")
+        {
+            List<Proizvod> pr = (List<Proizvod>)HttpContext.Application["proizvodi"];
+            List<Proizvod> proizvodi = new List<Proizvod>();
+            
+            
+                if (vrsta == "NAZIV" && nacin == "RASTUCE")
+                    proizvodi = pr.Where(p => p.BrojTegli > 0).OrderBy(p => p.Naziv).ToList<Proizvod>();
+                else if (vrsta == "NAZIV" && nacin == "OPADAJUCE")
+                    proizvodi = pr.Where(p => p.BrojTegli > 0).OrderByDescending(p => p.Naziv).ToList<Proizvod>();
+                else if (vrsta == "VRSTA MEDA" && nacin == "RASTUCE")
+                    proizvodi = pr.Where(p => p.BrojTegli > 0).OrderBy(p => p.Vrsta.ToString()).ToList<Proizvod>();
+                else if (vrsta == "VRSTA MEDA" && nacin == "OPADAJUCE")
+                    proizvodi = pr.Where(p => p.BrojTegli > 0).OrderByDescending(p => p.Vrsta.ToString()).ToList<Proizvod>();
+                else if (vrsta == "CENA" && nacin == "RASTUCE")
+                    proizvodi = pr.Where(p => p.BrojTegli > 0).OrderBy(p => p.CenaPoTegli).ToList<Proizvod>();
+                else if (vrsta == "CENA" && nacin == "OPADAJUCE")
+                    proizvodi = pr.Where(p => p.BrojTegli > 0).OrderByDescending(p => p.CenaPoTegli).ToList<Proizvod>();
+                else
+                    proizvodi = pr.Where(p => p.BrojTegli > 0).ToList<Proizvod>();  //ako ne unese nista ili unese sam nesto
+            
+
+            ViewBag.Proizvodi = proizvodi;
+            return View("Index"); 
+
+        }
+
+        //GET Proizvodi/PretragaPoNazivu
+        public ActionResult PretragaPoNazivu(string naziv)
+        {
+            List<Proizvod> pr = (List<Proizvod>)HttpContext.Application["proizvodi"];
+            List<Proizvod> proizvodi = new List<Proizvod>();
+            if(naziv=="")
+                proizvodi = pr.Where(p => p.BrojTegli > 0).ToList<Proizvod>();
+            else
+                proizvodi = pr.FindAll(p => p.Naziv.ToLower().Equals(naziv.ToLower())).Where(p => p.BrojTegli > 0).ToList<Proizvod>();
+            ViewBag.Proizvodi = proizvodi;
+            return View("Index");
+        }
+
+        //GET Proizvodi/PretragaPoVrsti
+        public ActionResult PretragaPoVrsti(string vrsta)
+        {
+            List<Proizvod> pr = (List<Proizvod>)HttpContext.Application["proizvodi"];
+            List<Proizvod> proizvodi = new List<Proizvod>();
+            if (vrsta == "")
+                proizvodi = pr.Where(p => p.BrojTegli > 0).ToList<Proizvod>();
+            else
+                proizvodi = pr.FindAll(p => p.Vrsta.ToString().Equals(vrsta)).Where(p => p.BrojTegli > 0).ToList<Proizvod>();
+            ViewBag.Proizvodi = proizvodi;
+            return View("Index");
+        }
+
+        //GET Proizvodi/PretragaPoCeni
+        public ActionResult PretragaPoCeni(int? minCena,int? maxCena)
+        {
+            List<Proizvod> pr = (List<Proizvod>)HttpContext.Application["proizvodi"];
+            List<Proizvod> proizvodi = new List<Proizvod>();
+            if (minCena==null || maxCena==null)
+                proizvodi = pr.Where(p => p.BrojTegli > 0).ToList<Proizvod>();
+            else            
+                proizvodi = pr.FindAll(p => p.CenaPoTegli >= minCena && p.CenaPoTegli<=maxCena).Where(p => p.BrojTegli > 0).ToList<Proizvod>();
+            ViewBag.Proizvodi = proizvodi;
+            return View("Index");
+        }
+
+
+
     }
 }
